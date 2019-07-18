e5dd0e322d2fe75360e25ff21abdf097651681d3
ganeshjagtap967 <32134927+ganeshjagtap967@users.noreply.github.com>
2019-07-18 19:59:52 +0530

    Complete REST implementation using Jersey

--- StudentRepository.java
+++ StudentRepository.java
@@ -1,92 +1,148 @@
 package com.example.restJersey;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.sql.*;
 
 public class StudentRepository {
-
-	List<Student> students;
 	
 	Connection con=null;
 	
-	public StudentRepository() throws SQLException, ClassNotFoundException {
-		
+	public StudentRepository() {
 		String url="jdbc:mysql://localhost:3306/new_schema";
 		String username="root";
 		String password="pass123";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+					
+			con=DriverManager.getConnection(url,username,password);
+				System.out.println("Connection established...");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		Class.forName("com.mysql.jdbc.Driver");
 		
-		con=DriverManager.getConnection(url,username,password);
-		
-		
-		students =new ArrayList<>();
-		
-				
 	}
 	
 	public List<Student> getStudents(){
 		List<Student> students=new ArrayList<>();
 		String sql="select * from test_table";
+		
 		try {
-		Statement st=con.createStatement();
+			Statement st=con.createStatement();
 		ResultSet rs=st.executeQuery(sql);
 		while(rs.next()) {
 			Student s=new Student();
 			s.setId(rs.getInt(1));
 			s.setName(rs.getString(2));
-			
+			System.out.println("Data fetched...");
 			students.add(s);
 			
 		}
-		}
-		catch(Exception e) {
-			System.out.println(e);
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 		return students;
+		//return null;
+		
 		
 	}
-	public Student getStudent(int id) {
-		String sql="select * from test_table where id=" +id;
-		Student s=new Student();
-		
+	
+	public Student getStudent(int id){
+String sql="select * from test_table where id=" +id;
+Student s=new Student();
+
 		try {
-		Statement st=con.createStatement();
+			Statement st=con.createStatement();
 		ResultSet rs=st.executeQuery(sql);
-		while(rs.next()) {
-			
+		if(rs.next()) {
 			s.setId(rs.getInt(1));
 			s.setName(rs.getString(2));
 			
-			
+			System.out.println("Record found...");
 			
 		}
-		}
-		catch(Exception e) {
+		
+		 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			System.out.println(e);
 		}
 		return s;
+		
+		
 		
 	}
 
 	public void create(Student s1) {
 		// TODO Auto-generated method stub
-		String sql="insert into test_table(?,?)";
+		String sql="insert into test_table values(?,?)";
+		
 		try {
 			PreparedStatement st=con.prepareStatement(sql);
-				st.setInt(1,s1.getId());
-				st.setString(2,s1.getName());
-				
-				st.executeUpdate();
-				
-			
-			}
-			catch(Exception e) {
-				System.out.println(e);
-			}
-		students.add(s1);
+	st.setInt(1,s1.getId());
+	st.setString(2,s1.getName());
+	st.executeUpdate();
+	
+		System.out.println("Record inserted...");
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
 		
 	}
+	
+	public void update(Student s1) {
+		// TODO Auto-generated method stub
+		String sql="update test_table set name=? where id=?";
+		
+		try {
+			PreparedStatement st=con.prepareStatement(sql);
+	
+	st.setString(1,s1.getName());
+	st.setInt(2,s1.getId());
+	st.executeUpdate();
+	System.out.println("Record updated...");
+		
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+	}
+
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+	String sql="delete from test_table where id=?";
+		
+		try {
+			PreparedStatement st=con.prepareStatement(sql);
+	
+	st.setInt(1,id);
+	st.executeUpdate();
+	System.out.println("Record deleted...");
+		
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
+}
+
+class St extends StudentRepository{
+	
 }
--- StudentResource.java
+++ StudentResource.java
@@ -1,12 +1,12 @@
 package com.example.restJersey;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,37 +15,63 @@ import javax.ws.rs.core.MediaType;
 @Path("/students")
 public class StudentResource {
 
-	//StudentRepository repo;
+	StudentRepository repo=new StudentRepository();
 	
 @GET
 //@Path("/students")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+
+
 	public List<Student> getStudents() {
-		System.out.println("studRes");
-		Student s1=new Student();
-		s1.setId(10);
-		s1.setName("Sonu");
 		
-		Student s2=new Student();
-		s2.setId(11);
-		s2.setName("Monu");
-		
-		List<Student> stud=Arrays.asList(s1,s2);
-		
-		return stud;
+		return repo.getStudents();
 }
 
-/*@POST
-@Consumes(MediaType.APPLICATION_JSON)
-public Student setStudent(Student s) {
-	int roll=s.getId();
-	String name=s.getName();
-//	System.out.println("Roll no:" + roll);
-//	System.out.println("Name:" +name);
-s.toString();
+@GET
+@Path("student/{id}")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public Student getStudent(@PathParam("id") int id) {
+	System.out.println(repo.getStudent(id));
+	return repo.getStudent(id);
+	
+}
 
+@POST
+@Path("student")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public Student createStudent(Student s1) {
+	repo.create(s1);
+	System.out.println(s1);
+	return s1;
+	
+}
+
+@PUT
+@Path("/student")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public Student updateStudent(Student s1) {
+	if(repo.getStudent(s1.getId()).getId()==0) {
+		repo.create(s1);
+	}
+	else {
+		repo.update(s1);
+	}
+	return s1;
+	
+}
+
+@DELETE
+@Path("/student/{id}")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public Student deleteStudent(@PathParam("id") int id) {
+	Student s=repo.getStudent(id);
+	if(s.getId()!=0) {
+		repo.delete(id);
+		}
+	else {
+		System.out.println("Record not present");
+	}
 	return s;
 	
 }
-*/
 }
